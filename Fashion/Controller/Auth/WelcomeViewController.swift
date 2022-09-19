5922df087bfbd8f784bc0c9024a668caca4908a9
Ahmed Yamany <ahmed.mohammed.yamany@gmail.com>
2022-09-19 18:25:31 +0200

    welcome screen

---
+++ Fashion/Controller/Auth/WelcomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WelcomeViewController.swift
+//  Fashion
+//
+//  Created by Ahmed Yamany on 19/09/2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    // MARK: - IBOutlets
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        welcomeLabel.text = NSLocalizedString("welcomeLabel", comment: "")
+        signUpButton.setTitle(NSLocalizedString("signUp", comment: ""), for: .normal)
+        loginButton.setTitle(NSLocalizedString("login", comment: ""), for: .normal)
+        loginButton.layer.cornerRadius = 20
+        signUpButton.layer.cornerRadius = 20
+    }
+}
